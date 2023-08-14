628424823a4ff6576cd723aef2be4a840c89dd27
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-08-14 13:49:04 +0530

    Jetcompose Textview

--- JetComposeApp/app/src/main/java/com/example/jetcomposeapp/MainActivity.kt
+++ JetComposeApp/app/src/main/java/com/example/jetcomposeapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.sp
 import com.example.jetcomposeapp.ui.theme.JetComposeAppTheme
 import com.example.jetcomposeapp.ui.theme.navokiColor
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -28,13 +35,38 @@ class MainActivity : ComponentActivity() {
                     color = navokiColor
 
                 ) {
-                    Greeting("Shivam")
+                    Column {
+                        Greeting("Shivam")
+                        DisplayText()
+                    }
                 }
             }
         }
     }
 
 }
+
+@Composable
+fun DisplayText(){
+
+    for (i in 1..10){
+        Text(text = "Display $i ${Random(0).nextInt(10)} \n " +
+                stringResource(id = R.string.app_name),
+            color = Color.Red,
+            fontSize = 30.sp,
+            style = TextStyle(
+                fontSize = 60.sp,
+                textDecoration = TextDecoration.LineThrough, background = Color.Yellow,
+                fontWeight = FontWeight.Bold,
+
+                )
+
+        )
+    }
+
+
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
