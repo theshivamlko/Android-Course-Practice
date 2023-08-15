b44771fecbf78dd1705bb2b89212c2a3d5f74e1e
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-08-15 23:30:59 +0530

    Atmic Variable

--- CoroutineMultiThread/app/src/main/java/com/example/coroutinemultithread/CocurrencySharedState.kt
+++ CoroutineMultiThread/app/src/main/java/com/example/coroutinemultithread/CocurrencySharedState.kt
@@ -5,15 +5,20 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.system.measureTimeMillis
 
 fun main() {
+
+  //  var counter=0 // OLD
+    //Solution
+    var counter=AtomicInteger(0)
 
     runBlocking {
         println("sharedState1 START")
         withContext(Dispatchers.Default){
             sharedState1{
-                counter++
+                counter.incrementAndGet()
             }
         }
         println("sharedState1 END $counter")
@@ -21,11 +26,31 @@ fun main() {
 }
 
 
-
+// Atomic Variable Solution
 suspend fun sharedState1(action:suspend()->Unit) {
 // run n*k times
     val n=100
-    val k=1000
+    val k=10000
+    val time= measureTimeMillis {
+        coroutineScope {
+            repeat(n){
+                launch {
+                    repeat(k){
+                        action()
+                    }
+                }
+            }
+        }
+    }
+
+    println("sharedState1 Completed ${n*k} times in $time ms")
+}
+
+// Fine Grained Solution
+suspend fun sharedState2(action:suspend()->Unit) {
+// run n*k times
+    val n=100
+    val k=10000
     val time= measureTimeMillis {
         coroutineScope {
             repeat(n){
