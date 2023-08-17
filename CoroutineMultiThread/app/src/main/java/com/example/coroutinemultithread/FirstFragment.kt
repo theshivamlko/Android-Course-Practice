ae59e63de059651435562ef55142a21fb258eff6
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-08-17 21:43:05 +0530

    save

--- CoroutineMultiThread/app/src/main/java/com/example/coroutinemultithread/FirstFragment.kt
+++ CoroutineMultiThread/app/src/main/java/com/example/coroutinemultithread/FirstFragment.kt
@@ -8,7 +8,14 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.coroutinemultithread.databinding.FragmentFirstBinding
 import kotlinx.coroutines.delay
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,18 +38,16 @@ class FirstFragment : Fragment() {
 
     }
 
-   @Volatile
-    var count: Int = 0
-
-    lateinit var atomiccount:AtomicInteger
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.button.setOnClickListener {
 
-            count=0
+            var count: Int = 0
+
+            count = 0
+
             val thread = Thread(Runnable {
                 println("Start")
                 for (i in 1..10000) {
@@ -56,25 +61,32 @@ class FirstFragment : Fragment() {
             })
 
 
-             val thread2 = Thread(Runnable {
+            val thread2 = Thread(Runnable {
                 println("Start2")
                 for (i in 1..10000) {
                     for (j in 1..100) {
                         count++
                     }
                 }
-                 println("END2 ${count}")
-             })
-            thread.start()
+                println("END2 ${count}")
+            })
+            val time = measureTimeMillis {
+                thread.run()
+            }
+            println("END2 time ${time}")
             println("run")
-            thread2.start()
+            val time2 = measureTimeMillis {
+                thread2.run()
+            }
+            println("END2 time2 ${time2}")
 
 
         }
 
         binding.button2.setOnClickListener {
+            lateinit var atomiccount: AtomicInteger
 
-            atomiccount=AtomicInteger(0)
+            atomiccount = AtomicInteger(0)
             val thread = Thread(Runnable {
                 println("Start")
                 for (i in 1..10000) {
@@ -103,17 +115,19 @@ class FirstFragment : Fragment() {
             println("run")
             thread2.start()
         }
-        var LOCK=Any()
 
         binding.button3.setOnClickListener {
+            var LOCK = Any()
 
-            var counter=0
+
+            var counter = 0
             val thread = Thread(Runnable {
                 println("Start")
                 for (i in 1..10000) {
                     for (j in 1..100) {
                         Thread.sleep(1000)
                         synchronized(LOCK) {
+
                             println("Thread1== ${counter++}")
                         }
 
@@ -142,6 +156,195 @@ class FirstFragment : Fragment() {
             thread.start()
 
             thread2.start()
+        }
+
+
+        binding.button4.setOnClickListener {
+            var counter = 0
+            val thread = Thread(Runnable {
+                println("Start")
+                for (i in 1..100) {
+                    for (j in 1..100) {
+                        println("Thread1== ${counter++}")
+                    }
+                }
+                println("")
+                println("END1 ${counter}")
+            })
+
+            val thread2 = Thread(Runnable {
+                println("Start2")
+                for (i in 1..100) {
+                    for (j in 1..100) {
+                        println("Thread2 ${counter++}")
+                    }
+                }
+                println("END2 ${counter}")
+            })
+            thread.start()
+
+            // wait some condition is meet else wait and sleep, then should exit
+            while (true) {
+                if (counter == 10000) {
+                    println("PASSED")
+                    Thread.sleep(2000)
+                    break;
+                } else if (counter > 10000) {
+                    println("INFINITE")
+                    break;
+                }
+            }
+            thread2.start()
+        }
+
+
+        binding.button5.setOnClickListener {
+            var counter = 0
+            val thread = Thread(Runnable {
+                println("Start")
+                for (i in 1..100) {
+                    for (j in 1..100) {
+                        println("Thread1== ${counter++}")
+                    }
+                }
+                println("")
+                println("END1 ${counter}")
+            })
+
+            val thread2 = Thread(Runnable {
+                println("Start2")
+                for (i in 1..100) {
+                    for (j in 1..100) {
+                        println("Thread2 ${counter++}")
+                    }
+                }
+                println("END2 ${counter}")
+            })
+
+
+            thread.start()
+            thread.join() // wait thread to finish
+            println("Main join ${thread.state}  ")
+
+
+            thread2.start()
+            println("Main returns  ")
+
+        }
+
+
+        binding.button6.setOnClickListener {
+
+            /*
+            var executor: ExecutorService = Executors.newCachedThreadPool(ThreadFactory {
+                 println("ExecutorService ${it.toString()}")
+                 Thread(it)
+             })
+             */
+
+
+            //   var executor: ExecutorService = Executors.newFixedThreadPool(1)
+            // Limits MAX no. of threads to use
+
+
+            val executor: ThreadPoolExecutor =
+                ThreadPoolExecutor(1, 2, 60, TimeUnit.SECONDS, ArrayBlockingQueue<Runnable>(10),
+                    ThreadFactory {
+                        println("ExecutorService ${it}")
+                        Thread(it)
+                    })
+
+
+
+            var counter = 0
+
+
+            repeat(5) {
+                executor.execute {
+                    val time = measureTimeMillis {
+                        println("Start")
+                        for (i in 1..10000) {
+                            for (j in 1..100) {
+                                counter++
+                                //  println("Thread1== ${counter++}")
+                            }
+                        }
+                    }
+                    println("END1 ${counter} $time")
+                }
+            }
+
+
+            executor.execute {
+                println("Start2")
+                val time = measureTimeMillis {
+
+                    for (i in 1..10000) {
+                        for (j in 1..100) {
+                            counter++
+                            //  println("Thread2 ${counter++}")
+                        }
+                    }
+                }
+                println("END2 ${counter} $time")
+            }
+
+
+
+            println("Main returns  ")
+
+        }
+
+        binding.button7.setOnClickListener {
+
+
+
+            val executor: ThreadPoolExecutor =
+                ThreadPoolExecutor(1, 2, 60, TimeUnit.SECONDS, ArrayBlockingQueue<Runnable>(10),
+                    ThreadFactory {
+                        println("ExecutorService ${it}")
+                        Thread(it)
+                    })
+
+
+
+            var counter = 0
+
+
+            repeat(5) {
+                executor.execute {
+                    val time = measureTimeMillis {
+                        println("Start")
+                        for (i in 1..10000) {
+                            for (j in 1..100) {
+                                counter++
+                                //  println("Thread1== ${counter++}")
+                            }
+                        }
+                    }
+                    println("END1 ${counter} $time")
+                }
+            }
+
+
+            executor.execute {
+                println("Start2")
+                val time = measureTimeMillis {
+
+                    for (i in 1..10000) {
+                        for (j in 1..100) {
+                            counter++
+                            //  println("Thread2 ${counter++}")
+                        }
+                    }
+                }
+                println("END2 ${counter} $time")
+            }
+
+
+
+            println("Main returns  ")
+
         }
 
     }
