65e787ec67726354f55e43f4b40db39973f8cbb3
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-08-19 17:42:26 +0530

    ChainStyle

--- JetComposeApp/app/src/main/java/com/example/jetcomposeapp/ConstraintlayoutActivity4.kt
+++ JetComposeApp/app/src/main/java/com/example/jetcomposeapp/ConstraintlayoutActivity4.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstrainedLayoutReference
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.example.jetcomposeapp.ui.theme.JetComposeAppTheme
@@ -38,35 +43,70 @@ class ConstraintlayoutActivity4 : ComponentActivity() {
 
 @Composable
 fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    ConstraintLayout {
+    Column {
+        ConstraintLayout {
+            val (red, blue, yellow) = createRefs()
 
-       val (red,blue,yellow)=createRefs()
+            Box(modifier = Modifier
+                .background(Color.Red)
+                .size(100.dp)
+                .constrainAs(red) {
 
-        Box(modifier=Modifier.background(Color.Red).size(100.dp).constrainAs(red){
+                    //  width= Dimension.matchParent
+                    //  width= Dimension.fillToConstraints
+                    height = Dimension.value(200.dp)
 
-           //  width= Dimension.matchParent
-           //  width= Dimension.fillToConstraints
-            height=Dimension.value(200.dp)
+                    // TO center child
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
 
-            // TO center child
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
+                })
 
-        })
+            Box(modifier = Modifier
+                .background(Color.Yellow)
+                .size(70.dp)
+                .constrainAs(yellow) {
+                    top.linkTo(red.bottom)
+                })
 
-        Box(modifier=Modifier.background(Color.Yellow).size(70.dp).constrainAs(yellow){
-            top.linkTo(red.bottom)
-        })
+            Box(modifier = Modifier
+                .background(Color.Blue)
+                .size(50.dp)
+                .constrainAs(blue) {
+                    start.linkTo(yellow.end)
+                    start.linkTo(red.end)
+                    top.linkTo(red.bottom)
 
-        Box(modifier=Modifier.background(Color.Blue).size(50.dp).constrainAs(blue){
-            start.linkTo(yellow.end)
-            start.linkTo(red.end)
-            top.linkTo(red.bottom)
 
+                })
 
-        })
+        }
+
+        Divider(thickness = 2.dp)
+
+        ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+            val (cyan, black, green) = createRefs()
+
+            Box(modifier = Modifier
+                .size(60.dp)
+                .background(Color.Cyan).constrainAs(cyan){})
+
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(60.dp).constrainAs(black){})
+
+            Box(modifier = Modifier
+                .background(Color.Green)
+                .size(60.dp).constrainAs(green){})
+
+            createHorizontalChain(cyan,black, green,chainStyle = ChainStyle.SpreadInside)
+
+        }
+
 
 
     }
+
+
 }
 
