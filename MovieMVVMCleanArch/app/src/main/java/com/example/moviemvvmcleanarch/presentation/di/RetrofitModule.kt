9e635fecc618c77707953d074fb153f774c37281
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-08-30 02:02:11 +0530

    add dagger at retrofit

---
+++ MovieMVVMCleanArch/app/src/main/java/com/example/moviemvvmcleanarch/presentation/di/RetrofitModule.kt
@@ -0,0 +1,26 @@
+package com.example.moviemvvmcleanarch.presentation.di
+
+import com.example.moviemvvmcleanarch.data.api.TMDBService
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class RetrofitModule(val url: String) {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTMDBService(retrofit: Retrofit): TMDBService {
+        return retrofit.create(TMDBService::class.java)
+    }
+}
