e753c8b9e430dbafedde23cb1a6687d76726f3f2
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-09-01 23:06:41 +0530

    save compose

--- UnitConvertorCompose/app/src/main/java/com/example/unitconvertorcompose/presentation/compose/BaseScreen.kt
+++ UnitConvertorCompose/app/src/main/java/com/example/unitconvertorcompose/presentation/compose/BaseScreen.kt
@@ -1,5 +1,7 @@
 package com.example.unitconvertorcompose.presentation.compose
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
@@ -15,12 +17,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.unitconvertorcompose.data.model.Conversion
+import com.example.unitconvertorcompose.data.model.ConversionResult
 import com.example.unitconvertorcompose.presentation.compose.convertor.ConvertorViewModel
 import com.example.unitconvertorcompose.presentation.compose.convertor.ConvertorViewModelFactory
 import com.example.unitconvertorcompose.presentation.compose.convertor.TopScreen
@@ -35,9 +42,34 @@ fun BaseScreen(
     convertorViewModel: ConvertorViewModel = viewModel(factory = factory)
 ) {
 
+    val configuration = LocalConfiguration.current
     val list = convertorViewModel.getConversions()
     val historyList = convertorViewModel.getHistoryList().collectAsState(initial = emptyList())
-    Column {
+
+
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+
+            getLandScape(convertorViewModel = convertorViewModel, list, historyList)
+
+        }
+
+        Configuration.ORIENTATION_PORTRAIT -> {
+            getPortrait(convertorViewModel = convertorViewModel, list, historyList)
+
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun getLandScape(
+    convertorViewModel: ConvertorViewModel,
+    list: List<Conversion>,
+    historyList:  State<List<ConversionResult>>,
+) {
+    return Column {
         TopAppBar(
             title = {
                 Text(
@@ -56,7 +88,8 @@ fun BaseScreen(
                 }
             }
         )
-        Column(modifier = modifier.padding(20.dp)) {
+        Row(modifier = Modifier.padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
 
             TopScreen(
                 list,
@@ -80,4 +113,59 @@ fun BaseScreen(
         }
 
     }
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun getPortrait(
+    convertorViewModel: ConvertorViewModel,
+    list: List<Conversion>,
+    historyList: State<List<ConversionResult>>,
+) {
+    return Column {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Unit Convertor App", style = TextStyle(
+                        color = Color.White
+                    )
+                )
+            },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = Color.Blue),
+            actions = {
+                IconButton(onClick = { convertorViewModel.deleteAllResults() }) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh, contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+            }
+        )
+        Column(modifier = Modifier.padding(20.dp)) {
+
+            TopScreen(
+                list,
+                convertorViewModel.selectConversion,
+                convertorViewModel.inputText,
+                convertorViewModel.typedValue,
+                convertorViewModel.outputResult
+            ) { s1, s2 ->
+                convertorViewModel.addResult(s1, s2)
+
+            }
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            if (historyList.value.isNotEmpty()) {
+                HistoryScreen(historyList) {
+                    convertorViewModel.deleteResult(it)
+                }
+            } else {
+                Text(text = "No data available")
+            }
+        }
+
+    }
+
 }
