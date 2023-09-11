2b3f7f4166363596446fdf46fa4fc06f28385689
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-09-11 20:55:40 +0530

    registerForActivityResult, new file with Intent

--- Storage/app/src/main/java/com/example/storage/MainActivityExternalStorage.kt
+++ Storage/app/src/main/java/com/example/storage/MainActivityExternalStorage.kt
@@ -1,6 +1,8 @@
 package com.example.storage
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -8,19 +10,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -28,10 +37,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.storage.ui.theme.StorageTheme
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.lifecycleScope
 import com.example.storage.Controller.Companion.deletePhotoFromInternalStorage
 import com.example.storage.Controller.Companion.loadPhotoFromInternalStorage
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
 
 lateinit var cameraPermissionLauncher: ActivityResultLauncher<String>
 
@@ -39,13 +53,35 @@ lateinit var galleryLauncher: ManagedActivityResultLauncher<String, List<@JvmSup
 
 class MainActivityExternalStorage : ComponentActivity() {
 
+    lateinit var activityResult: ActivityResultLauncher<Intent>
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
-        loadPhotoFromInternalStorage(baseContext)
+        activityResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                println("registerForActivityResult2 ${result.resultCode} ${result.data}")
+                println("registerForActivityResult2  ${result.data?.data}")
+                if (result.resultCode == Activity.RESULT_OK) {
+                    result.data?.data?.let {
+                        contentResolver.openOutputStream(it)?.apply {
+                            val text: String = "Some random Text ${
+                                SimpleDateFormat("dd-MM-yyy hh:mm").format(
+                                    Date(System.currentTimeMillis())
+                                )
+                            }"
+                            write(text.encodeToByteArray())
+                        }
+                    };
 
+                }
+
+
+            }
+
+        loadPhotoFromInternalStorage(baseContext)
 
         setContent {
             StorageTheme {
@@ -62,8 +98,9 @@ class MainActivityExternalStorage : ComponentActivity() {
                     uriList.forEach {
                         //   val bitmap=BitmapFactory.decodeFile(it.path)
                         Controller.saveImageToExternalStorage(
-                            context, System.currentTimeMillis().toString(), MediaStore.Images
-                                .Media.getBitmap(context.contentResolver, it)
+                            context,
+                            System.currentTimeMillis().toString(),
+                            MediaStore.Images.Media.getBitmap(context.contentResolver, it)
                         )
 
                     }
@@ -72,18 +109,28 @@ class MainActivityExternalStorage : ComponentActivity() {
 
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
+                    Scaffold(topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Storage")
+                            },
+                            colors = TopAppBarDefaults.largeTopAppBarColors(containerColor = Color.Blue)
+                        )
+                    }, containerColor = Color.White) {
 
-                    Greeting("Android")
+                        Greeting("Android", paddingValues = it)
+
+                    }
+
                 }
             }
         }
     }
 
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
+    fun Greeting(name: String, modifier: Modifier = Modifier, paddingValues: PaddingValues) {
         val context = LocalContext.current
 
         var list = remember { mutableStateListOf<AppFile>() }
@@ -96,7 +143,8 @@ class MainActivityExternalStorage : ComponentActivity() {
                     val bytes = item?.readBytes()
                     if (bytes != null) {
                         Controller.writeFileExternalStorage(
-                            context, System.currentTimeMillis().toString(),bytes)
+                            context, System.currentTimeMillis().toString(), bytes
+                        )
                     }
 
                     /* Controller.saveImageToExternalStorage(
@@ -110,31 +158,31 @@ class MainActivityExternalStorage : ComponentActivity() {
 
 
 
-        Column(modifier = Modifier.padding(20.dp)) {
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+                .padding(paddingValues)
+        ) {
             Text(
                 text = "Hello $name!",
 
-                modifier = modifier,
-                fontSize = 20.sp
+                modifier = modifier, fontSize = 20.sp
             )
 
             Button(onClick = {
 
-                galleryLauncher.launch("image/*").apply {
-                }
+                galleryLauncher.launch("image/*").apply {}
 
             }) {
                 Text(
-                    text = "Read File",
-                    modifier = modifier
+                    text = "Read File", modifier = modifier
                 )
             }
             Button(onClick = {
 
             }) {
                 Text(
-                    text = "Write File",
-                    modifier = modifier
+                    text = "Write File", modifier = modifier
                 )
             }
             Button(onClick = {
@@ -142,20 +190,36 @@ class MainActivityExternalStorage : ComponentActivity() {
 
             }) {
                 Text(
-                    text = "Ask Permission",
-                    modifier = modifier
+                    text = "Ask Permission", modifier = modifier
+                )
+            }
+            Button(onClick = {
+
+
+                val intent = Intent()
+                intent.action = Intent.ACTION_CREATE_DOCUMENT
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_TITLE, "MyRandom File.docx")
+                println("registerForActivityResult1")
+                intent.putExtra("requestCode", 1);
+                activityResult.launch(intent)
+
+            }) {
+                Text(
+                    text = "Intent New File", modifier = modifier
                 )
             }
 
             LazyColumn() {
                 items(list.size) {
-                    Image(bitmap = list[it].bmp.asImageBitmap(), contentDescription = null,
+                    Image(bitmap = list[it].bmp.asImageBitmap(),
+                        contentDescription = null,
                         modifier = Modifier
                             .clickable {
                                 deletePhotoFromInternalStorage(list.get(it).name, context)
                             }
-                            .height(200.dp)
-                    )
+                            .height(200.dp))
                 }
             }
         }
